592d1d6515d79c93043b9c77215e0ae084ded0ec
mtso <mtso@mail.com>
2016-09-28 11:48:17 -0700

    Add icon and AF manager

--- Splash/ViewController.swift
+++ Splash/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let clientUrl = "https://api.unsplash.com/photos/?client_id=cfd0978593306a55d98b2ed41b174aed276da5a4f35a9d4dfd134fc2d654f817"
+        
+        let manager = AFHTTPSessionManager() //(baseURL: NSURL(string: clientUrl))
+        
+//        manager.requestSerializer = AFJSONRequestSerializer()
+        manager.responseSerializer = AFJSONResponseSerializer()
+        
+        manager.GET(
+            clientUrl,
+            parameters: nil,
+            success: { task, response in
+                print("JSON: \(response?.description)")
+            },
+            failure: { task, error in
+                print("ERROR: \(error.localizedDescription)")
+            })
     }
 
     override func didReceiveMemoryWarning() {
